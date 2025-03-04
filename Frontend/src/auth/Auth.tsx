d8ab5f04428c7f55efad3dce248c34d2659e5bbc
Junaid Khan <157748393+Junaidify@users.noreply.github.com>
2025-03-04 19:06:34 +0530

    login page

--- Frontend/src/auth/Auth.tsx
+++ Frontend/src/auth/Auth.tsx
@@ -1,35 +1,38 @@
-import '../styles/auth.css';
+import "../styles/auth.css";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import login_page from "../images/login_page.jpg";
 
 const Auth = () => {
   return (
     <div id="auth">
       <div className="auth_left">
-        <h3>DevOrbit</h3>
-        <h1>Log in</h1>
-        <p>
-          Don't have an account ? <button>create account</button>
-        </p>
+        <h3 className="auth_welcome">Welcome Back!</h3>
+        <div id="auth_container">
+          <p className="auth_account_created">
+            Don't have an account ?&nbsp;&nbsp;
+            <button className="auth_account_btn"> Create account</button>
+          </p>
 
-        <button>
-          <FontAwesomeIcon icon={faGoogle} style={{ color: "#6d603c" }} />{" "}
-          Continue with Google
-        </button>
-        <p>or</p>
+          <button className="auth_google">
+            <FontAwesomeIcon icon={faGoogle} style={{ color: "#6d603c" }} />{" "}
+            Continue with Google
+          </button>
+          <p className="auth_or">OR</p>
 
-        <div>
-          <div>
+          <div id="auth_input">
             <input type="text" placeholder="Email" />
             <input type="text" placeholder="Password" />
             <button>Log in</button>
           </div>
 
-          <p>Forget password ?</p>
+          <p className="auth_forget_btn">Forget password ?</p>
         </div>
       </div>
 
-      <div className="auth_right"></div>
+      <div className="auth_right">
+        <img src={login_page} alt="auth_login_page_image" />
+      </div>
     </div>
   );
 };
